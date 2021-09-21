679523ebfd8394ed127a2259e86839a5490d71ca
Callan <callan@wir.pl>
2021-09-21 03:12:27 +0200

    new export format; better errors handling; selecting imported route

--- RoutesImportExport.lua
+++ RoutesImportExport.lua
@@ -19,45 +19,125 @@ local route_zone_args_desc_table = {
     order = 0,
 }
 
+local function normaliseRouteData(routeData)
+    routeData.hidden = false
+    routeData.looped = 1
+    routeData.visible = true
+    routeData.selection = {}
+    routeData.db_type = {}
+    routeData.taboos = {}
+    routeData.taboolist = {}
+
+    return routeData
+end
+
+local function shallowCopy(from)
+    local to = {}
+    for k, v in pairs(from) do
+        to[k] = v
+    end
+    return to
+end
+
+local function stripSpecialChars(string)
+    return string:gsub("[;:+/=-.|]", " ")
+end
+
+local function getProjectName(projectId)
+    return (projectId == WOW_PROJECT_MAINLINE and "Retail") or (projectId == WOW_PROJECT_CLASSIC and "Classic") or (projectId == WOW_PROJECT_BURNING_CRUSADE_CLASSIC and "TBCC") or "Unknown"
+end
+
+local function getProjectLongName(projectId)
+    return (projectId == WOW_PROJECT_MAINLINE and "Retail WoW") or (projectId == WOW_PROJECT_CLASSIC and "Classic WoW") or (projectId == WOW_PROJECT_BURNING_CRUSADE_CLASSIC and "Burning Crusade Classic") or "Unknown WoW Type"
+end
+
+local function getProjectId(projectName)
+    return (projectName =="Retail" and WOW_PROJECT_MAINLINE) or (projectName == "Classic" and WOW_PROJECT_CLASSIC) or (projectName == "TBCC" and WOW_PROJECT_BURNING_CRUSADE_CLASSIC)
+end
+
+local function serialize(data)
+    local projectName = getProjectName(WOW_PROJECT_ID)
+    local location = stripSpecialChars(C_Map.GetMapInfo(data.RouteZone).name)
+    local name = stripSpecialChars(data.RouteName)
+
+    return string.format("%s:Routes:%s:%s:%s", projectName, name, location, LibBase64.Encode(AceSerializer:Serialize(data)))
+end
+
+local function deserialize(exportString)
+    local sections = { strsplit(":", exportString) }
+
+    if #sections == 0 then return nil end
+
+    if #sections == 5 and getProjectId(sections[1]) ~= WOW_PROJECT_ID then
+        print(string.format("|cFFFF0a0aThis import string is for %s not %s|r", getProjectLongName(getProjectId(sections[1])), getProjectLongName(WOW_PROJECT_ID)))
+        return nil
+    end
+
+    return AceSerializer:Deserialize(LibBase64.Decode(sections[#sections]))
+end
+
+local function importRouteData(data)
+    if data and data.RouteZone and data.RouteKey and data.RouteName and data.RouteData then
+        local mapInfo = C_Map.GetMapInfo(data.RouteZone)
+        if not mapInfo then return end
+
+        normaliseRouteData(data.RouteData)
+
+        Routes.db.global.routes[data.RouteZone][data.RouteName] = nil
+        Routes.db.global.routes[data.RouteZone][data.RouteName] = data.RouteData
+
+        local opts = Routes.options.args.routes_group.args
+        zoneKey = tostring(data.RouteZone)
+        routeKey = data.RouteKey
+
+        if not opts[zoneKey] then
+            local mapName = mapInfo.name
+
+            opts[zoneKey] = {
+                type = "group",
+                name = mapName,
+                desc = L["Routes in %s"]:format(mapName),
+                args = {
+                    desc = route_zone_args_desc_table,
+                },
+            }
+
+            Routes.routekeys[data.RouteZone] = {}
+        end
+
+        Routes.routekeys[data.RouteZone][routeKey] = data.RouteName
+        opts[zoneKey].args[routeKey] = Routes:GetAceOptRouteTable()
+
+        LibStub("AceConfigDialog-3.0"):SelectGroup('Routes', 'routes_group', zoneKey, data.RouteKey)
+
+        print("Route " .. data.RouteName .. " (" .. mapInfo.name .. ") imported succefully" )
+
+        return true
+    end
+
+    return false
+end
+
 local function importRoute(value)
     if strlen(value) > 0 then
         local routes = { strsplit(";", value) }
         local updated = false
+        local zoneKey, routeKey
 
         for _, route in pairs(routes) do
             route = strtrim(route)
 
             if strlen(route) > 0 then
-                local result, data = AceSerializer:Deserialize(LibBase64.Decode(route))
+                local result, data = deserialize(route)
 
-                if result and data and data.RouteZone and data.RouteKey and data.RouteName and data.RouteData then
-                    Routes.db.global.routes[data.RouteZone][data.RouteName] = nil
-                    Routes.db.global.routes[data.RouteZone][data.RouteName] = data.RouteData
-
-                    local opts = Routes.options.args.routes_group.args
-                    local zoneKey = tostring(data.RouteZone)
-
-                    if not opts[zoneKey] then
-                        local mapName = C_Map.GetMapInfo(data.RouteZone).name
-
-                        opts[zoneKey] = {
-                            type = "group",
-                            name = mapName,
-                            desc = L["Routes in %s"]:format(mapName),
-                            args = {
-                                desc = route_zone_args_desc_table,
-                            },
-                        }
-
-                        Routes.routekeys[data.RouteZone] = {}
+                if result and data.Routes then
+                    for _, currentRoute in pairs(data.Routes) do
+                        if importRouteData(currentRoute) then
+                            updated = true
+                        end
                     end
-
-                    Routes.routekeys[data.RouteZone][data.RouteKey] = data.RouteName
-                    opts[zoneKey].args[data.RouteKey] = Routes:GetAceOptRouteTable()
-
+                elseif result and importRouteData(data) then
                     updated = true
-
-                    print("Route " .. data.RouteName .. " (" .. C_Map.GetMapInfo(data.RouteZone).name .. ") imported succefully" )
                 end
             end
         end
@@ -101,6 +181,8 @@ local function renameRoute(zone, routeKey, newName)
 
     Routes:DrawWorldmapLines()
     Routes:DrawMinimapLines(true)
+
+    LibStub("AceConfigDialog-3.0"):SelectGroup('Routes', 'routes_group', zoneKey, newRouteKey)
 end
 
 local renameGroup = {
@@ -132,9 +214,9 @@ local exportGroup = {
         local routekey = info[3]
         local name = Routes.routekeys[zone][routekey]
         local route = Routes.db.global.routes[zone][name]
-        local data = { RouteZone = zone, RouteKey = routekey, RouteName = name, RouteData = route }
+        local data = { RouteZone = zone, RouteKey = routekey, RouteName = name, RouteData = normaliseRouteData(shallowCopy(route)) }
 
-        return LibBase64.Encode(AceSerializer:Serialize(data))
+        return serialize(data)
     end,
     order = 999,
 }
